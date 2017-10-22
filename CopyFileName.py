599919c84b9824abeb5cab5730ff3f8428eaaba2
ashish3x3 <aks133844@gmail.com>
2017-10-22 23:40:24 +0530

    adding copy filename plugin

---
+++ CopyFileName.py
@@ -0,0 +1,14 @@
+import sublime
+import sublime_plugin
+import os
+
+class CopyfilenameCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		if len(self.view.file_name()) > 0:
+			filename = os.path.split(self.view.file_name())[1]
+			sublime.set_clipboard(filename)
+			sublime.status_message("Copied file name: %s" % filename)
+
+	def is_enabled(self):
+		# return self.view.file_name() and len(self.view.file_name()) > 0
+		return True
